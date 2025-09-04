80ac4490832e04b0497902bec6ca9c0d7325a946
MayurAghao92 <mayuraghao54@gmail.com>
2025-09-04 13:17:29 +0530

    Implemented Gemini

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import app from './src/app.js';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
@@ -6,7 +8,14 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
-  // ...
+  console.log("connected");
+  socket.on("disconnect",()=>{
+    console.log(" disconnected");
+  })
+  socket.on("message",(data)=>{
+    console.log("message received");
+    console.log(data);
+  })
 });
 
 httpServer.listen(5000, () => {
--- src/app.js
+++ src/app.js
@@ -2,5 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
 
 export default app;
